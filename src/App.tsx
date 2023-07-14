9fa644c80c481e5ce02b3a3238fbacd92cee2461
Ryan Griggs <griggsspace@gmail.com>
2023-07-14 10:02:31 -0700

    template for react app using TS and WebP

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,5 @@ const App: React.FC  = () => {
     <h1>App Loaded</h1>
   );
 };
-import React from 'react';
-import Board from './components/Board';
 
-
-const App: React.FC  = () => {
-  console.log('App rendered');
-  return (
-    <div id="appContainer">
-      <h1>Tic-Tac-Toe</h1>
-      <Board />
-    </div>
-  );
-};
+export default App;
